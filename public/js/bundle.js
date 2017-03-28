7e818edeaf3918d1c1c1bc89aad41a0949b92c12
Kris Redding <kris.redding3@gmail.com>
2017-03-28 09:45:58 -0400

    finish proof 2

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -13,7 +13,7 @@ workBenchApp.config(['$stateProvider', '$urlRouterProvider','$locationProvider',
         url: "/",
         views: {
           'content':{
-            component: 'clintonP2'
+            component: 'clintonP1'
           }
         }
       })
@@ -107,6 +107,31 @@ components.component('clintonMinistries', {
    templateUrl: 'views/clinton/ministries.html'
 });
 
+components.component('clintonP1', {
+  bindings: {},
+	require: {
+      parent: '^clinton'
+  },
+	controller: function () {
+      var ctrl = this;
+
+      ctrl.homeImg = "imgs/clinton/p4.jpg";
+
+
+      ctrl.churchInfo = { "address":"223 Elizabeth Avenue Rockville, MD 20850", "phone":"301-340-7942", "fax":"301-825-8938", "email":"camez223@aol.com", "welcome":"Our vision is to affect a completely transformed community of faith, worthy of citizenship in the Kingdom of God, by the power of God through His Son Jesus Christ. Our mission is to produce ministries that will serve the CAUSE of Christ through the spiritual transformation of minds, bodies and spirits. Sunday morning worship services begin at 10:00 AM." };
+      ctrl.churchGallery = ["imgs/clinton/p1.jpg", "imgs/clinton/p8.jpg", "imgs/clinton/p2.jpg", "imgs/clinton/p7.jpg", "imgs/clinton/p3.jpg", "imgs/clinton/p6.jpg"];
+      ctrl.upcomingEvents = [{"date":new Date("2016-03-25 21:00:00"), "title":"Chorale Ensemble Choir Rehearsal"},{"date":new Date("2016-03-27 19:00:00"), "title":"TIPS Bible Study @Dorothy Day Place"},{"date":new Date("2016-03-25 19:30:00"), "title":"Children & Youth Choir Rehearsal"}  ];
+
+      ctrl.connectedCards = [
+        {"title":"Watch Live", "img":"imgs/clinton/p6.jpg", "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+        {"title":"Small Groups", "img":"imgs/clinton/subimgs/s2.jpg", "content":"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
+        {"title":"Church Services", "img":"imgs/clinton/Clinton_2012.jpg", "content":"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."},
+        {"title":"Bible Study", "img":"imgs/clinton/subimgs/biblestudy.jpg", "content":"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+      ];
+   },
+   templateUrl: 'views/clinton/home1.html'
+});
+
 components.component('clintonP2', {
   bindings: {},
 	require: {
@@ -184,6 +209,34 @@ components.component('clintonP3', {
       ctrl.upcomingEvents = [{"date":new Date("2016-03-25 21:00:00"), "title":"Chorale Ensemble Choir Rehearsal"},{"date":new Date("2016-03-27 19:00:00"), "title":"TIPS Bible Study @Dorothy Day Place"},{"date":new Date("2016-03-25 19:30:00"), "title":"Children & Youth Choir Rehearsal"}  ];
    },
    templateUrl: 'views/clinton/home3.html'
+});
+
+components.component('cltHeader1', {
+  bindings: {},
+	require: {
+      parent: '^clinton'
+  },
+	controller: function () {
+      var ctrl = this;
+
+      ctrl.navigation = [
+        {"title":"About Us", "subSections":[{"title":"Our History","image":"imgs/clinton/subimgs/history.jpg"}, {"title":"Our Vision","image":"imgs/clinton/subimgs/vision.jpg"},{"title":"Our Staff","image":"imgs/clinton/subimgs/staff.jpg"},{"title":"Service Times","image":"imgs/clinton/subimgs/time.jpg"},{"title":"Inside Zion","image":"imgs/clinton/subimgs/zion1.jpg"}]},
+        {"title":"Get Connected", "subSections":[]},
+        {"title":"Ministries", "subSections":[
+          {"title":"Administration","image":"imgs/clinton/subimgs/history.jpg"},{"title":"Bible Study","image":"imgs/clinton/subimgs/biblestudy.jpg"}, {"title":"Children/Youth Ministries","image":"imgs/clinton/subimgs/youth1.jpg"},
+          {"title":"Christian Education","image":"imgs/clinton/subimgs/christian1.jpg"},{"title":"Community Collaborations for Outreach","image":"imgs/clinton/subimgs/community.jpg"},{"title":"Health Ministry","image":"imgs/clinton/subimgs/health1.jpg"},
+          {"title":"Lay Council","image":"imgs/clinton/subimgs/biblestudy.jpg"},{"title":"Men's Outreach","image":"imgs/clinton/subimgs/men1.jpg"},{"title":"Small Group Fellowship","image":"imgs/clinton/subimgs/smallGroup.jpg"}
+        ]},
+        {"title":"Our Pastor", "subSections":[]},
+        {"title":"Media", "subSections":[]},
+        {"title":"News & Events", "subSections":[]}
+      ];
+
+      ctrl.selectedNav = ctrl.navigation[2];
+
+      ctrl.mainImg = "imgs/clinton/p4.jpg";
+   },
+   templateUrl: 'views/clinton/templates/header2.html'
 });
 
 components.component('cltHeader2', {

38d4ddf1dda65b5fa13b4863f526c7abd14baab9
Kris Redding <kris.redding3@gmail.com>
2018-02-28 16:56:56 -0500

    update search

--- app/js/components/searchBar.component.js
+++ app/js/components/searchBar.component.js
@@ -1,9 +1,22 @@
 components.component('searchbar', {
-    bindings: {},
-      require: {},
-      controller: function () {
-        var ctrl = this;
-     },
-     templateUrl: 'views/templates/_search.html'
-  });
+  bindings: {},
+  require: {},
+    controller: function () {
+      var ctrl = this;
+
+      /*
+        Search Codes
+        0 = Default
+        1 = Search Page
+       */
+      ctrl.pageView = 0;
+
+      ctrl.pageChange = function(type){
+        if(ctrl.pageView != type){
+          ctrl.pageView = type;
+        }
+      }
+  },
+  templateUrl: 'views/templates/_search.html'
+});
   
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -102,13 +102,26 @@ components.component('home', {
 });
 
 components.component('searchbar', {
-    bindings: {},
-      require: {},
-      controller: function () {
-        var ctrl = this;
-     },
-     templateUrl: 'views/templates/_search.html'
-  });
+  bindings: {},
+  require: {},
+    controller: function () {
+      var ctrl = this;
+
+      /*
+        Search Codes
+        0 = Default
+        1 = Search Page
+       */
+      ctrl.pageView = 0;
+
+      ctrl.pageChange = function(type){
+        if(ctrl.pageView != type){
+          ctrl.pageView = type;
+        }
+      }
+  },
+  templateUrl: 'views/templates/_search.html'
+});
   
 components.component('uc', {
     bindings: {},
